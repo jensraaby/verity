ed4f5cf0c5894cef30f466fb36f55cd2ceb68a11
Jens Raaby <jens+github@m.raaby.co.uk>
2014-08-13 15:12:20 +0100

    Moved walker

--- goverify.go
+++ goverify.go
@@ -87,29 +87,3 @@ func exists(path string) bool {
 	}
 	return true
 }
-
-/*
- Basically, exploring a directory structure is a matter of walking through a tree with a breadth-first-search. Go has a facility for this: the filepath.Walk function and WalkFunc type.
- Here I will try and use the latter to print the mod-time of everything in a dir.
-*/
-func mtimePrinter(path string, info os.FileInfo, err error) error {
-	// there is a special error SkipDir we can use to avoid expanding dirs
-
-	// if bytes.HasPrefix([]byte(path), []byte(ignoredir)) {
-	// 	fmt.Println("WARNING: Path skipping, has ignoredir as prefix")
-	// 	fmt.Println("Ignoredir:", ignoredir)
-	// 	return filepath.SkipDir
-	// }
-	f, err := os.Stat(path)
-	if err != nil {
-		fmt.Println("Error stat'ing path,", path)
-	}
-
-	fmt.Println("Path", path, "Modification time:", f.ModTime())
-	return nil
-}
-
-func printStuff(startpath string) error {
-	filepath.Walk(startpath, mtimePrinter)
-	return nil
-}
--- hashing/hashing.go
+++ hashing/hashing.go
@@ -6,6 +6,8 @@ package hashing
 import (
 	"crypto/sha1"
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -28,6 +30,32 @@ type DirHash struct {
 func HashDir(path string) error {
 	fmt.Println(path)
 	data := []byte("This page intentionally left blank.")
-	fmt.Printf("% x", sha1.Sum(data))
+	fmt.Printf("% x\n", sha1.Sum(data))
+	return nil
+}
+
+/*
+ Basically, exploring a directory structure is a matter of walking through a tree with a breadth-first-search. Go has a facility for this: the filepath.Walk function and WalkFunc type.
+ Here I will try and use the latter to print the mod-time of everything in a dir.
+*/
+func mtimePrinter(path string, info os.FileInfo, err error) error {
+	// there is a special error SkipDir we can use to avoid expanding dirs
+
+	// if bytes.HasPrefix([]byte(path), []byte(ignoredir)) {
+	// 	fmt.Println("WARNING: Path skipping, has ignoredir as prefix")
+	// 	fmt.Println("Ignoredir:", ignoredir)
+	// 	return filepath.SkipDir
+	// }
+	f, err := os.Stat(path)
+	if err != nil {
+		fmt.Println("Error stat'ing path,", path)
+	}
+
+	fmt.Println("Path", path, "Modification time:", f.ModTime())
+	return nil
+}
+
+func printStuff(startpath string) error {
+	filepath.Walk(startpath, mtimePrinter)
 	return nil
 }
